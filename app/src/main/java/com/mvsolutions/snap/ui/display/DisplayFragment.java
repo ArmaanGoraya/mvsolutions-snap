74df779a515e0e4b3ba277893d59eb520162cb06
ArmaanGoraya <armannoodles@gmail.com>
2020-08-25 12:57:13 -0400

    a commit

--- app/src/main/java/com/mvsolutions/snap/ui/display/DisplayFragment.java
+++ app/src/main/java/com/mvsolutions/snap/ui/display/DisplayFragment.java
@@ -1,30 +1,30 @@
 package com.mvsolutions.snap.ui.display;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.ColorRes;
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
+import androidx.appcompat.graphics.drawable.DrawableWrapper;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.mvsolutions.snap.R;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class DisplayFragment extends Fragment {
-
-    private DisplayViewModel DisplayViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,8 +33,7 @@ public class DisplayFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DisplayViewModel =
-                ViewModelProviders.of(this).get(DisplayViewModel.class);
+        com.mvsolutions.snap.ui.display.DisplayViewModel displayViewModel = ViewModelProviders.of(this).get(DisplayViewModel.class);
         View root = inflater.inflate(R.layout.fragment_display, container, false);
 
         assert getArguments() != null;
@@ -44,22 +43,32 @@ public class DisplayFragment extends Fragment {
             View selectorHeader = root.findViewById(R.id.SelectorHeader);
             Button dynamicButton = new Button(this.getContext());
             dynamicButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-            dynamicButton.setVisibility(View.VISIBLE);
+            //Edited this line to see if I can make the corners of the dynamic buttons rounded
+            //If error occurs delete line
+            dynamicButton.setBackgroundResource(R.drawable.rounded_corners);
+            dynamicButton.setTextColor(Color.WHITE);
             dynamicButton.setText(txtBlb.get(i));
 
             LinearLayout linearLayout = root.findViewById(R.id.SelectorLinearLayout);
             linearLayout.addView(dynamicButton);
+
+            TextView txt = new TextView(this.getContext());
+            txt.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            txt.setTextSize(7);
+            linearLayout.addView(txt);
         }
-        Button dynamicButton = new Button(this.getContext());
-        dynamicButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        dynamicButton.setText("Click to manually  enter");
+        Button typeOwn = new Button(this.getContext());
+        typeOwn.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        typeOwn.setText("Click to manually enter");
+        typeOwn.setBackgroundResource(R.drawable.rounded_corners);
+        typeOwn.setTextColor(Color.WHITE);
         final LinearLayout linearLayout = root.findViewById(R.id.SelectorLinearLayout);
-        linearLayout.addView(dynamicButton);
-        dynamicButton.setOnClickListener(new View.OnClickListener() {
+        linearLayout.addView(typeOwn);
+        typeOwn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 linearLayout.removeAllViews();
-                final TextInputEditText txt = new TextInputEditText(getContext());
+                final TextInputEditText txt = new TextInputEditText(Objects.requireNonNull(getContext()));
                 txt.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 linearLayout.addView(txt);
                 Button btn = new Button(getContext());
@@ -69,7 +78,7 @@ public class DisplayFragment extends Fragment {
                 btn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        String text = txt.getText().toString();
+                        String text = Objects.requireNonNull(txt.getText()).toString();
                         Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
                     }
                 });
